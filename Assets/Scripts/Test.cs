5b977433dd11146725ad7561b83030385716f6d2
nathan <nathan@nathan.com>
2016-01-29 19:39:08 -0800

    test rotation

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public float direction = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(new Vector3(0,0,30*Time.deltaTime*direction));
+        }
+	}
+}
